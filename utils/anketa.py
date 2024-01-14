ace549a621d40ac1eae5028370e3532ff261d11b
Sergey Ilin <ilin_sergey90@mail.ru>
2024-01-14 20:52:23 +0300

    add experience keyboard

--- utils/anketa.py
+++ utils/anketa.py
@@ -1,4 +1,4 @@
-from telegram import Update
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import ContextTypes, ConversationHandler
 
 from data_base.user import set_vacancy
@@ -11,8 +11,28 @@ async def anketa_start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> st
     return "vacancy_name"
 
 
-async def save_vacancy(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+async def save_vacancy(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
     text = update.message.text
     set_vacancy(update.effective_user, text)  # type: ignore [arg-type]
-    await update.message.reply_text(f"Ваканcии будут искаться по запросу {text}")
+    keyboard = [
+        [
+            InlineKeyboardButton("Нет опыта", callback_data="noExperience"),
+        ],
+        [
+            InlineKeyboardButton("От 1 года до 3 лет", callback_data="between1And3"),
+        ],
+        [
+            InlineKeyboardButton("От 3 до 6 лет", callback_data="between3And6"),
+        ],
+        [
+            InlineKeyboardButton("Более 6 лет", callback_data="moreThan6"),
+        ],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text("Выбери опыт работы:", reply_markup=reply_markup)
+    return "experience"
+
+
+async def save_experience(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    await update.message.reply_text("Данные для поиска сохранены!")
     return ConversationHandler.END
